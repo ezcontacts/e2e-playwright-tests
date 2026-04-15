830696d7a0540e9b5999f7347bd3a3a7ac4bf9a9
deepakvermaezcontacts <deepak@ezcontacts.com>
2026-04-15 12:02:51 +0530

    Resolving confilcts

--- test/steps/pages/clContactLensPage.step.ts
+++ test/steps/pages/clContactLensPage.step.ts
@@ -1,29 +1,23 @@
-import { Given, test, Then, When } from "../../fixtures/fixture"
+import { Given, test, Then, When } from "../../fixtures/fixture";
 
 Given(
   "the user lands on the Contact Lenses detail page",
   async ({ CLContactLensPage }) => {
     await CLContactLensPage.open();
-  }
+  },
 );
 
 Then(
-  'the page should display the text as {string}',
+  "the page should display the text as {string}",
   async ({ CLContactLensPage }, text: string) => {
     await CLContactLensPage.verifyTextVisible(text);
-  }
+  },
 );
 
-Then(
-  "the product brand should be visible",
-  async ({ CLContactLensPage }) => {
-    await CLContactLensPage.verifyProductBrandVisible();
-  }
-);
+Then("the product brand should be visible", async ({ CLContactLensPage }) => {
+  await CLContactLensPage.verifyProductBrandVisible();
+});
 
-Then(
-  "the product name should be visible",
-  async ({ CLContactLensPage }) => {
-    await CLContactLensPage.verifyProductNameVisible();
-  }
-);
+Then("the product name should be visible", async ({ CLContactLensPage }) => {
+  await CLContactLensPage.verifyProductNameVisible();
+});

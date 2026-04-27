54d27b7d6dd91d757fdd525b89dae1cb1af172c3
Nikunj Pitroda <nikunjp@ezcontacts.com>
2026-04-27 18:45:24 +0530

    EZSANISOFT-5382-Code changes done

--- page-objects/pages/Account/AddressAndPaymentPage.ts
+++ page-objects/pages/Account/AddressAndPaymentPage.ts
@@ -1,43 +1,78 @@
 import { expect, Locator, Page } from "@playwright/test";
 import { AccountPage } from "./AccountPage";
 import { ENDPOINT } from "../../../constant/endpoint";
+import { generatePaymentData, generateUSAddress } from "../../../utils/user_helper";
 
 export class AddressAndPaymentPage extends AccountPage {
 
     readonly cardNumberTextbox: Locator;
-    readonly expiryMonthLocator: Locator;
-    readonly expiryYearLocator: Locator;
-    readonly buttonSaveChanges: Locator;
+    readonly expMonthLocator: Locator;
+    readonly expYearLocator: Locator;
+    readonly btnSaveChanges: Locator;
 
+    readonly AddressFirstName: Locator;
+    readonly AddressLastName: Locator; 
+    readonly AddressLine1: Locator;
+    readonly AddressCity: Locator;
+    readonly AddressState: Locator;
+    readonly AddressZip: Locator;
+    readonly AddressPhone: Locator;
     
-  constructor(page: Page) {
-    super(page, ENDPOINT.addressAndPayment);
+    constructor(page: Page) {
+        super(page, ENDPOINT.addressAndPayment);
 
     this.cardNumberTextbox = page.getByRole('textbox', { name: 'Credit Card Number' });
-    this.expiryMonthLocator = page.locator('#AccountCardTokenExpiryMonth');
-    this.expiryYearLocator = page.locator('#AccountCardTokenExpiryYear');
-    this.buttonSaveChanges = page.getByRole('button', { name: 'Save Changes' }) //Save changes button on add card details form
+    this.expMonthLocator = page.locator('#AccountCardTokenExpiryMonth');
+    this.expYearLocator = page.locator('#AccountCardTokenExpiryYear');
+    this.btnSaveChanges = page.getByRole('button', { name: 'Save Changes' }) //Save changes button on add card details form
+    
+    // Locators for My account -> Shipping and Billing address.
+    this.AddressFirstName = page.getByRole('textbox', { name: 'First Name' }) 
+    this.AddressLastName = page.getByRole('textbox', { name: 'Last Name' })
+    this.AddressLine1 = page.getByRole('textbox', { name: 'Address line 1' })
+    this.AddressCity = page.getByRole('textbox', { name: 'City' })
+    this.AddressState = page.getByRole('combobox', { name: 'State' })
+    this.AddressZip = page.getByRole('textbox', { name: 'Zip Code' })
+    this.AddressPhone = page.getByRole('textbox', { name: 'Phone Number' })
+    }
 
-
-}
     async clickAddLink(type: string): Promise<void> {
        await this.page.getByRole('link', { name: `Add ${type}` }).click();
-
     }
 
-    async verifyFormVisible(formName: string) {
-        await expect(this.page.getByText(formName)).toBeVisible();
+    async verifyFormDisplayed(formName: string): Promise<void> {
+        const heading = this.page.getByRole('heading', { name: formName });
+        await expect(heading, `Form not visible: ${formName}`).toBeVisible();
     }
 
-    async enterCardDetails() {
-        await this.cardNumberTextbox.fill('378282246310005');
-        await this.expiryMonthLocator.selectOption('11');
-        await this.expiryYearLocator.selectOption('26');
+    async enterCardDetails(cardNumber: string, month: string, year: string): Promise<void> {
+        const data = generatePaymentData();
+        await this.cardNumberTextbox.fill(data.cardNumber);
+        await this.expMonthLocator.selectOption(data.expiryMonth);
+        await this.expYearLocator.selectOption(data.expiryYear);
     }
 
-    async clickButton(buttonName: string) {
-       // await this.page.getByRole('button', { name: buttonName }).click();
-        await this.buttonSaveChanges.click();
+    async clickButton() {
+        await this.btnSaveChanges.click();
     }
 
+    getSuccessMessageByText(text: string) {
+        return this.page.getByText(text);
+    }
+
+    async verifySuccessfulMessage(text: string): Promise<void> {
+        const message = this.getSuccessMessageByText(text);
+        await expect(message).toBeVisible();
+    }
+
+    async FillCustomerAddressForm() {
+        const addressData = generateUSAddress();
+        await this.AddressFirstName.fill(addressData.firstName);
+        await this.AddressLastName.fill(addressData.lastName);
+        await this.AddressLine1.fill(addressData.address1);
+        await this.AddressCity.fill(addressData.city);
+        await this.AddressState.selectOption(({ value: 'New York' }));
+        await this.AddressZip.fill(addressData.zip);
+        await this.AddressPhone.fill(addressData.phone);
+    }
 }
--- test/features/account/address_payment.feature
+++ test/features/account/address_payment.feature
@@ -1,3 +1,5 @@
+@EZSANISOFT-5382
+
 Feature: Address and Payment Management
 
 Background:
@@ -15,30 +17,20 @@ Examples:
   | Billing Address  | Add Billing Address    | /account/address-and-payment/add-billing-address     |
   | Payment Method   | Add Card Details       | /account/address-and-payment/add-card-details        |
 
-
 Scenario Outline: Verify user can add a new card
-  Given the user is on the "<formName>" form
+  When the user clicks on Add "Payment Method" link
   When the user enters card details with "<cardNumber>", "<expiryMonth>", and "<expiryYear>"
   And the user clicks on the "<buttonName>" button
-  Then the new card details should be displayed in the "<sectionName>" section
+  And a "Your card details have been added successfully." message should be displayed
 
-Examples:
-  | formName         | cardNumber        | expiryMonth | expiryYear | buttonName   | sectionName     |
-  | Add Card Details | 4111111111111111  | 12          | 26         | Save Changes | Payment Methods |
+Scenario: Verify that user can save the Shipping Address
+  When the user clicks on Add "Shipping Address" link
+  Then the user enters shipping address details  
+  And the user clicks on the "<buttonName>" button
+  And a "The account shipping address has been saved." message should be displayed
 
-
-@skip
-Scenario Outline: Verify saved addresses are displayed with actions
-  Then all saved "<addressType>" addresses should be visible
-  And each "<addressType>" address should have the following actions:
-    | Action |
-    | Edit   |
-    | Delete |
-
-
-@skip
-Scenario: Verify Address and Payment Methods actions
-  Then each saved address and payment method should have the following actions:
-    | Action |
-    | Edit   |
-    | Delete |
+Scenario: Verify that user can save the Billing Address
+  When the user clicks on Add "Billing Address" link
+  Then the user enters billing address details
+  And the user clicks on the "<buttonName>" button
+  And a "The account billing address has been saved." message should be displayed
